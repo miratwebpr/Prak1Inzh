efa6528021b860f0d10b3ff818cd22f264920610
spapua228М <spapua228@mail.ru>
2022-03-25 19:20:09 +0500

    2 and 3

--- Prak1Inzh/Prak1Inzh.cpp
+++ Prak1Inzh/Prak1Inzh.cpp
@@ -1,11 +1,18 @@
 ﻿#include <GL/glew.h>
 #include <Gl/freeglut.h>
-
+#include <glm/vec3.hpp>
+GLuint VBO;
 static void RenderSceneCB()
 {
     glClear(GL_COLOR_BUFFER_BIT);
+    glEnableVertexAttribArray(0);
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, 0);
+    glDrawArrays(GL_TRIANGLES, 0, 3);
+    glDisableVertexAttribArray(0);
     glutSwapBuffers();
 }
+
 int main(int argc, char* argv[])
 {
     glutInit(&argc, argv);
@@ -14,10 +21,28 @@ int main(int argc, char* argv[])
     glutInitWindowPosition(100, 100);
     glutCreateWindow("Tutorial 01");
     glutDisplayFunc(RenderSceneCB);
+    
+
+    GLenum res = glewInit();
+
+    if (res != GLEW_OK)
+    {
+        fprintf(stderr, "Error: '%s'\n", glewGetErrorString(res));
+        return 1;
+    }
+    //glm::vec3 Vertices[1];
+    //Vertices[0] = glm::vec3(0.0f, 0.0f, 0.0f);
+    glm::vec3 Vertices[3];
+    Vertices[0] = glm::vec3(1.0f, 1.0f, 0.0f);
+    Vertices[1] = glm::vec3(1.0f, -1.0f, 0.0f);
+    Vertices[2] = glm::vec3(-1.0f, 0.0f, 0.0f);
+    glGenBuffers(1, &VBO);
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(Vertices), Vertices, GL_STATIC_DRAW);
+
+
     glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
     glutMainLoop();
-    glClear(GL_COLOR_BUFFER_BIT);
-    glutSwapBuffers();
 
     return 0;
 }
