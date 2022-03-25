569ffeddf9ea41bfe12cc7b4c9b038f208a8bc6f
spapua228М <spapua228@mail.ru>
2022-03-25 17:48:06 +0500

    Добавьте файлы проекта.

---
+++ Prak1Inzh/Prak1Inzh.cpp
@@ -0,0 +1,23 @@
+﻿#include <GL/glew.h>
+#include <Gl/freeglut.h>
+
+static void RenderSceneCB()
+{
+    glClear(GL_COLOR_BUFFER_BIT);
+    glutSwapBuffers();
+}
+int main(int argc, char* argv[])
+{
+    glutInit(&argc, argv);
+    glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGBA);
+    glutInitWindowSize(1024, 768);
+    glutInitWindowPosition(100, 100);
+    glutCreateWindow("Tutorial 01");
+    glutDisplayFunc(RenderSceneCB);
+    glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
+    glutMainLoop();
+    glClear(GL_COLOR_BUFFER_BIT);
+    glutSwapBuffers();
+
+    return 0;
+}
